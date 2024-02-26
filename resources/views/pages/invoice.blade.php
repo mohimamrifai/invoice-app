776aa58f4d3f7aabd5d6a33916664a9d309372ff
mohimamrifai <imamrifaibisnis@gmail.com>
2024-02-26 21:29:08 +0700

    ui: memperbaiki tampilan halaman invoice form

--- resources/views/pages/invoice.blade.php
+++ resources/views/pages/invoice.blade.php
@@ -15,20 +15,22 @@
                 <input name="bill_to" id="bill_to" class="border-blue-500 border-2 outline-none p-2 rounded-md" type="text" placeholder="Jhon Doe">
             </div>
         </div>
-        <div class="flex flex-col gap-1 mt-2">
+        <h3 class="mt-3 font-bold text-sm"> + ADD INVOICE ITEM</h3>
+        <div class="flex flex-col gap-1">
             <label for="keterangan">Keterangan</label>
             <textarea class="outline-none border-blue-500 border-2 p-2 rounded-md" placeholder="Text here..." name="keterangan" id="keterangan"></textarea>
         </div>
         <div class="mt-3 flex gap-3">
             <div class="flex flex-col gap-1 w-8/12">
                 <label for="harga" class="text-base text-gray-600">Harga :</label>
-                <input name="harga" id="harga" class="border-blue-500 border-2 outline-none p-2 rounded-md" type="number" placeholder="20000">
+                <input name="harga" id="harga" class="border-blue-500 border-2 outline-none p-2 rounded-md" type="number" min="0" placeholder="0">
             </div>
             <div class="flex flex-col gap-1 w-2/12">
                 <label for="jumlah" class="text-base text-gray-600">Jumlah :</label>
-                <input name="jumlah" id="jumlah" class="border-blue-500 border-2 outline-none p-2 rounded-md" type="number" placeholder="2">
+                <input name="jumlah" min="1" value="1" id="jumlah" class="border-blue-500 border-2 outline-none p-2 rounded-md" type="number" placeholder="0">
             </div>
         </div>
+        <button type="button" id="tambah_item" class="mt-4 bg-blue-500 py-1 px-3 rounded text-white font-bold hover:bg-blue-400 transition-all duration-200">Tambah</button>
     </form>
     {{-- tampilan langsung --}}
     <div class="w-6/12 border-l-2 border-gray-800 ps-5 pt-1">
@@ -49,27 +51,15 @@
                     <p class="text-sm" id="">12/12/2024</p>
                 </div>
             </div>
-            <table class="w-full text-left mt-3 text-xs">
-                <tr class="border-b-2 pb-2">
-                    <th>KETERANGAN</th>
-                    <th>HARGA</th>
-                    <th>JML</th>
-                    <th>TOTAL</th>
-                </tr>
-                <tr>
-                    <td>Pulpen</td>
-                    <td>5000</td>
-                    <td>3</td>
-                    <td>15000</td>
-                </tr>
-                <tr>
-                    <td>Buku</td>
-                    <td>20000</td>
-                    <td>2</td>
-                    <td>40000</td>
+            <table id="table_preview" class="w-full text-justify mt-3 text-xs">
+                <tr class="border-b-2 pb-2 mb-2 bg-gray-900 text-slate-100">
+                    <th class="ps-2 w-7/12 text-left">KETERANGAN</th>
+                    <th class="w-2/12">HARGA</th>
+                    <th class="w-1/12 ">JML</th>
+                    <th class="w-2/12 text-center">TOTAL</th>
+                    <th class="w-2/12 text-center pe-2">ACT</th>
                 </tr>
             </table>
-
         </div>
     </div>
 </div>
@@ -79,10 +69,76 @@
     const billFrom = document.getElementById('bill_from');
     const billTo = document.getElementById('bill_to');
     const formCreate = document.getElementById('form_create');
+    const btnTambahItem = document.getElementById('tambah_item');
+    const keterangan = document.getElementById('keterangan');
+    const harga = document.getElementById('harga');
+    const jumlah = document.getElementById('jumlah');
+    const tablePreview = document.getElementById('table_preview')
 
-    formCreate.addEventListener('input', function() {
-        document.getElementById('preview_bill_from').innerText = billFrom.value;
+    const invoiceItems = [];
+
+    billTo.addEventListener('input', function() {
         document.getElementById('preview_bill_to').innerText = billTo.value;
+    });
+
+    billFrom.addEventListener('input', function() {
+        document.getElementById('preview_bill_from').innerText = billFrom.value;
+    });
+
+    function deleteRow(button) {
+        var row = button.parentNode.parentNode; // Dapatkan baris (tr) yang berisi tombol yang diklik
+        row.parentNode.removeChild(row); // Hapus baris dari tabel
+    }
+
+    btnTambahItem.addEventListener('click', function() {
+        const item = {
+            keterangan: keterangan.value
+            , harga: parseInt(harga.value)
+            , jumlah: parseInt(jumlah.value)
+            , total: parseInt(harga.value) * parseInt(jumlah.value)
+        }
+        invoiceItems.push(item)
+
+        while (tablePreview.rows.length > 1) {
+            tablePreview.deleteRow(1);
+        }
+
+        invoiceItems.forEach((item) => {
+            const row = document.createElement('tr');
+            row.classList.add('border-b-2', 'pb-2', 'mb-2', 'bg-gray-100', 'text-sm');
+
+            const cell1 = document.createElement('td');
+            cell1.classList.add('ps-3', 'text-left');
+            cell1.textContent = item.keterangan;
+            row.appendChild(cell1);
+
+            const cell2 = document.createElement('td');
+            cell2.textContent = item.harga;
+            row.appendChild(cell2);
+
+            const cell3 = document.createElement('td');
+            cell3.classList.add('text-center');
+            cell3.textContent = item.jumlah;
+            row.appendChild(cell3);
+
+            const cell4 = document.createElement('td');
+            cell4.classList.add('me-2', 'text-center');
+            cell4.textContent = item.total;
+            row.appendChild(cell4);
+
+            const cell5 = document.createElement('td');
+            cell5.classList.add('text-red-500', 'font-bold');
+            const deleteButton = document.createElement('button');
+            deleteButton.textContent = 'X';
+            deleteButton.onclick = function() {
+                deleteRow(this); // Panggil fungsi deleteRow saat tombol diklik
+            };
+            cell5.appendChild(deleteButton);
+            row.appendChild(cell5);
+
+
+            tablePreview.appendChild(row);
+        });
 
     });
 
